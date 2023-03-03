8e752867c606c71a62bf53095ba7ccad3d03937f
TAZPaltaps <diane.reixach@ensam.eu>
2023-03-03 18:19:38 +0100

    Initial commit

---
+++ merchex/listings/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>A propos</h1> <p> Nous adorons merch!</p>')
